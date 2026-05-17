aaea47d804f8444601753bc876640a1c8eb67813
bccdx <30099072@cc.peralta.edu>
2026-05-17 07:14:49 -0700

    Add Gradebook implementation
    Add GradeBook implementation

---
+++ GradeBook.cpp
@@ -0,0 +1,26 @@
+#include "GradeBook.h"
+
+GradeBook::GradeBook()
+{
+
+}
+
+void GradeBook::addGrade(const std::string& assignment, double score)
+{
+
+}
+
+double GradeBook::getAverage() const
+{
+
+}
+
+double GradeBook::getHighest() const
+{
+
+}
+
+int GradeBook::numGrades() const
+{
+
+}
